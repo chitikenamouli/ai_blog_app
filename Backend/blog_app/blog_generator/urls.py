8a630266415a0a761f0523e638225809f069b180
chitikenamouli <chitikenamouli@gmail.com>
2024-05-18 15:00:56 +0530

    Initial commit

---
+++ Backend/blog_app/blog_generator/urls.py
@@ -0,0 +1,9 @@
+
+# blog_generator/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
---
+++ Backend/blog_app/blog_generator/views.py
@@ -0,0 +1,8 @@
+
+# blog_generator/views.py
+
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
